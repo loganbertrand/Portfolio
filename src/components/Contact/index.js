a1af68ed69a63c94585fa4f4e0fcf9affb8c6eac
Logan Bertrand <loganwbertrand@gmail.com>
2019-12-05 16:50:24 -0800

    css fixes

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,42 +7,46 @@ import "./contact.css"
 const Contact = () => {
     return (
         <div className='contact-section'>
-
+                <div className='test'>
                 <Grid item xs={12}>
                     <a href="mailto:loganwbertrand@gmail.com" target="_blank" rel="noopener noreferrer">
                         <div className="circle"><i className="fas fa-3x fa-envelope"></i></div>
                     </a>
                     <h1 className='title'>Email</h1>
                 </Grid>
-
+                </div>
+                <div className='test'>
                 <Grid item xs={12}>
                     <a href={Pdf} target="_blank" rel="noopener noreferrer">
                         <div className="circle"><i className="fas fa-3x fa-file-alt"></i></div>
                     </a>
                     <h1 className='title'>Resume</h1>
                 </Grid>
-
+                </div>
+                <div className='test'>
                 <Grid item xs={12}>
                     <a href="#">
                         <div className="circle"><i className="fas fa-3x fa-mobile-alt"></i></div>
                     </a>
                     <h1 className='title'>(760) 822-1629</h1>
                 </Grid>
-
+                </div>
+                <div className='test'>
                 <Grid item xs={12}>
                     <a href="https://www.linkedin.com/in/logan-bertrand-/" target="_blank" rel="noopener noreferrer">
                         <div className="circle"><i className="fab fa-3x fa-linkedin"></i></div>
                     </a>
                     <h1 className='title'>LinkedIn</h1>
                 </Grid>
-
+                </div>
+                <div className='test'>
                 <Grid item xs={12}>
                     <a href="https://github.com/loganbertrand" target="_blank" rel="noopener noreferrer">
                         <div className="circle"><i className="fab fa-3x fa-github"></i></div>
                     </a>
                     <h1 className='title'>GitHub</h1>
                 </Grid>
-
+                </div>
         </div>
     )
 }
